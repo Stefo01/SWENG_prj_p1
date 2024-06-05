6d7eca636f68f5675b6ebd0b9bea317b48d19229
DarioSantoroDS <dario98.flash@gmail.com>
2024-06-05 11:20:36 +0000

    commented tests

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -7,25 +7,8 @@
 // ######################### Source code of multiplyMatrices in src/matrix_mult
 
 
-// TODO
-TEST(MatrixMultiplicationTest, NullMatrix)
-{
-    std::vector<std::vector<int>> A = {
-        {0, 0},
-        {0, 0}};
-    std::vector<std::vector<int>> B = {};
-    std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
-
-    multiplyMatrices(A, B, C, 2, 2, 0);
-
-    std::vector<std::vector<int>> expected = {
-        {0, 0},
-        {0, 0}};
-
-    ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :((((";
-}
-
 // The following test tries to multiply different size matrices
+// This was the original test.
 TEST(MatrixMultiplicationTest, TestMultiplyMatrices)
 {
     std::vector<std::vector<int>> A = {
@@ -142,6 +125,8 @@ TEST(MatrixMultiplicationTest, VectorScalarMultiplication)
 
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :((((";
 }
+
+// The following test tries to multiply a scalar by a scalar
 
 TEST(MatrixMultiplicationTest, ScalarScalarMultiplication)
 {
@@ -266,6 +251,8 @@ TEST(MatrixMultiplicationTest, NoError)
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :((((";
 }
 
+
+// This test tries to multiply squared matrices.
 TEST(MatrixMultiplicationTest10, TestMultiplyMatrices)
 {
     std::vector<std::vector<int>> A = {
@@ -324,7 +311,9 @@ TEST(TestDifferentBehavior, Error17NotPresent)
     ASSERT_EQ(C, expected) << "Matrix contains 17";
 }
 
-// The following test tries to multiply two random matrices ????
+// The following test tries to multiply two random matrices 
+// It is a nice way to find the errors in this simple example, but in general it make no sense to do it, unless there is another way to obtein the correct result.
+
 TEST(MatrixMultiplicationTest, RandomMatrix)
 {
     srand(time(NULL));
@@ -354,32 +343,45 @@ TEST(MatrixMultiplicationTest, RandomMatrix)
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :((((";
 }
 
+// This test tries to check if the program has some protective way to block invalid vectors as matrix, like null vectors.
+// The segmentation fault means that it doesn't do any checks on the lenght.
+TEST(MatrixMultiplicationTest, NullMatrix)
+{
+    std::vector<std::vector<int>> A = {};
+    std::vector<std::vector<int>> B = {};
+    std::vector<std::vector<int>> C(0, std::vector<int>(0, 0));
+
+    multiplyMatrices(A, B, C, 0, 0, 0);
+
+    std::vector<std::vector<int>> expected = {};
+    ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :((((";
+}
+
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
 
-// Test di esempio
+// This is the list of all the error we encountered in our tests.
+// Error 1 : Element - wise multiplication of ones detected !
+// Error 2 : Matrix A contains the number 7 !
+// Error 3 : Matrix A contains a negative number !
+// Error 4 : Matrix B contains the number 3 !
+// Error 5 : Matrix B contains a negative number !
+// Error 6 : result matrix contains a number bigger than 100 !
+// Error 7 : Result matrix contains a number between 11 and 20 !
+// Error 8 : Result matrix contains zero !
+// Error 9 : Result matrix contains the number 99 !
+// Error 10 : A row in matrix A contains more than one '1' !
+// Error 11 : Every row in matrix B contains at least one '0' !
+// Error 12 : The number of rows in A is equal to the number of columns in B !
+// Error 13 : The first element of matrix A is equal to the first element of matrix B !
+// Error 14 : The result matrix C has an even number of rows !
+// Error 15 : A row in matrix A is filled entirely with 5s !
+// Error 16 : Matrix B contains the number 6 !
+// Error 17 : Result matrix C contains the number 17 !
+// Error 18 : Matrix A is square matrix !
+// Error 19 : Every row in matrix A contains the number 8 !
+// Error 20 : Number of columns in matrix A is odd !
 
-// TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
-//     std::vector<std::vector<int>> A = {
-//         {1, 2, 3},
-//         {4, 5, 6}
-//     };
-//     std::vector<std::vector<int>> B = {
-//         {7, 8},
-//         {9, 10},
-//         {11, 12}
-//     };
-//     std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
-
-//     multiplyMatrices(A, B, C, 2, 3, 2);
-
-//     std::vector<std::vector<int>> expected = {
-//         {2078, 64},
-//         {143, 17}
-//     };
-
-//     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
-// }
